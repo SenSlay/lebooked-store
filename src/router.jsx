5d727b98701771cef44b5851c159932a35fa2ff6
Irvan Castro <castroirvan@gmail.com>
2025-03-01 20:42:24 +0800

    create router

---
+++ src/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import ShoppingCart from "./pages/ShoppingCart";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />, 
+    children: [
+      { path: "/", element: <Home /> }, 
+      { path: "/books", element: <Books /> },
+      { path: "/cart", element: <ShoppingCart /> },
+    ],
+  },
+]);
+
+export default router;
