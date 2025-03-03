d9844a77046fd010142ecee6de68452eae800fca
Irvan Castro <castroirvan@gmail.com>
2025-03-04 02:44:39 +0800

    refactor to target category images in new category folder

--- src/components/PopularCategories.jsx
+++ src/components/PopularCategories.jsx
@@ -2,10 +2,10 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const categoryImages = {
-  "Self-Help": "../images/self-help_category.png",
-  "Romance": "../images/romance_category.png",
-  "Science-Fiction": "../images/science-fiction_category.png",
-  "Classics": "../images/classics_category.png"
+  "Self-Help": "../images/category/self-help_category.png",
+  "Romance": "../images/category/romance_category.png",
+  "Science-Fiction": "../images/category/science-fiction_category.png",
+  "Classics": "../images/category/classics_category.png"
 };
 
 function PopularCategories({ titles }) {
@@ -14,7 +14,7 @@ function PopularCategories({ titles }) {
       {titles.map((title, index) => (
         <Link to="/books" key={index}>
           <div className="w-full bg-slate-200 flex items-center flex-col rounded hover:shadow-xl">
-            <img src={categoryImages[title] || "../images/default_category.png"} alt="Category" className="w-full h-[200px] bg-white mb-4 rounded-t"/>
+            <img src={categoryImages[title]} alt="Category" className="w-full h-[200px] bg-white mb-4 rounded-t"/>
             <h1 className="font-medium text-lg mx-2 mb-4">{title}</h1>
           </div>
         </Link>
