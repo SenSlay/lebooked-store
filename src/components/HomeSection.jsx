4d09713100c6ff85e163879cd7002f19ab2c9ca6
Irvan Castro <castroirvan@gmail.com>
2025-03-04 02:36:09 +0800

    create HomeSection component to display each section in the Home Page

---
+++ src/components/HomeSection.jsx
@@ -0,0 +1,12 @@
+const HomeSection = ({ title, children }) => {
+  return (
+    <div className="flex px-2">
+      <section className="flex-1 flex max-w-7xl py-12 flex-col m-auto overflow-hidden">
+        <h1 className="font-semibold text-3xl mb-4">{title}</h1>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default HomeSection;
