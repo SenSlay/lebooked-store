17ad89ae31b5090d186387667936a1093039cd90
Irvan Castro <castroirvan@gmail.com>
2025-03-04 02:31:19 +0800

    create BookCard component to display book details

---
+++ src/components/BookCard.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function BookCard({ book }) {
+  return (
+    <Link to={`/books/${book.id}`}>
+      <div className="flex flex-col w-[200px] lg:w-[250px] min-h-[450px] text-wrap p-4 bg-slate-200 rounded">
+          <img src={book.image} alt="" className="bg-white w-[180px] h-[250px] px-2 self-center mb-2"/>
+          <h2 className="text-lg font-semibold">{book.title}</h2>
+          <h3 className="mb-auto text-gray-700">{book.author}</h3>
+          
+          <p className="text-xl font-semibold mb-1">${book.price}</p>
+        <button onClick={(e) => e.preventDefault()} className="border-2 border-solid border-blue-600 uppercase text-blue-600 text-lg font-semibold hover:bg-blue-600 hover:text-white p-0.5">ADD TO CART</button>
+      </div>
+    </Link>
+  )
+}
+
+export default BookCard;
