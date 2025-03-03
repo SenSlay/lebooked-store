95692ab6570de40bcc322f8f492c9d9e1ed78d94
Irvan Castro <castroirvan@gmail.com>
2025-03-04 02:38:51 +0800

    create Home page
    - Added hero, popular categories, trending, and bestsellers sections
    - Only applied initial styling- still incomplete styling and functionality

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+// import { useEffect, useState } from "react";
+import Carousel from "../components/Carousel";
+import HomeSection from "../components/HomeSection";
+import { BooksContext } from "../BooksContext";
+import { useContext } from "react";
+import PopularCategories from "../components/PopularCategories";
+
+function Home() {
+  const { books, loading, error } = useContext(BooksContext);
+
+  if (error) return <p>Error: {error}</p>;
+  
+  const bestsellers = books.filter(book => book.tags.includes("Bestseller"));
+  const trending = books.filter(book => book.tags.includes("Trending"));
+
+  return (
+    <main className="h-full">
+        <div className="relative bg-[url('../images/hero.jpg')] bg-no-repeat bg-cover bg-center before:absolute before:inset-0 before:bg-black/60 h-[650px] flex justify-center items-center ">
+          <div className="relative z-10 p-6 text-white text-center max-w-[700px] min-w-[500px]">
+            <h1 className="font-bold text-7xl text-blue-300 mb-4">LeBooked</h1>
+            <p className="font-medium text-2xl">Your Next Favorite Read Awaits.</p>
+            <button className="">Shop Now</button>
+          </div>
+        </div>
+        <HomeSection title="Popular Categories">
+          <PopularCategories titles={['Self-Help', 'Romance', 'Science-Fiction', 'Classics']}></PopularCategories>
+        </HomeSection>
+      
+        <HomeSection title="What's Trending">
+          {loading ? <p>Loading...</p> : <Carousel books={trending} />}
+        </HomeSection>
+
+        <HomeSection title="Bestsellers">
+          {loading ? <p>Loading...</p> : <Carousel books={bestsellers} />}
+        </HomeSection>
+      </main>
+  );
+}
+
+export default Home;
