f4bece0a558e95cb729031b580514eb28c15f986
Irvan Castro <castroirvan@gmail.com>
2025-03-04 21:32:50 +0800

    improve styling on hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 // import { useEffect, useState } from "react";
 import Carousel from "../components/Carousel";
 import HomeSection from "../components/HomeSection";
-import { BooksContext } from "../BooksContext";
+import { BooksContext } from "../context/BooksContext";
 import { useContext } from "react";
 import PopularCategories from "../components/PopularCategories";
+import { Link } from "react-router-dom";
 
 function Home() {
   const { books, loading, error } = useContext(BooksContext);
@@ -16,10 +17,12 @@ function Home() {
   return (
     <main className="h-full">
         <div className="relative bg-[url('../images/hero.jpg')] bg-no-repeat bg-cover bg-center before:absolute before:inset-0 before:bg-black/60 h-[650px] flex justify-center items-center ">
-          <div className="relative z-10 p-6 text-white text-center max-w-[700px] min-w-[500px]">
-            <h1 className="font-bold text-7xl text-blue-300 mb-4">LeBooked</h1>
-            <p className="font-medium text-2xl">Your Next Favorite Read Awaits.</p>
-            <button className="">Shop Now</button>
+          <div className="relative z-10 p-6 text-white text-center min-w-[60%] flex flex-col items-center">
+            <h1 className="font-bold text-7xl text-blue-500 mb-3">LeBooked</h1>
+            <p className="font-medium text-2xl mb-5">Your Next Favorite Read Awaits</p>
+            <Link to='/books' className="border-2 border-white text-white px-4 py-2 text-lg font-semibold w-[200px] block uppercase hover:bg-white hover:text-blue-600 rounded-sm">
+              Shop Now
+            </Link>
           </div>
         </div>
         <HomeSection title="Popular Categories">
