7e2c25cf72ed599bce55f9de9dfdeca0ac572186
Irvan Castro <castroirvan@gmail.com>
2025-03-08 01:07:38 +0800

    add overflow-visible on larger screens for left/right carousel arrows

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,7 +1,7 @@
 const HomeSection = ({ title, children }) => {
   return (
     <div className="flex px-2">
-      <section className="flex-1 flex max-w-7xl py-12 flex-col m-auto overflow-hidden">
+      <section className="flex-1 flex max-w-7xl py-12 flex-col m-auto overflow-hidden xl:overflow-visible">
         <h1 className="font-semibold text-3xl mb-4">{title}</h1>
         {children}
       </section>
