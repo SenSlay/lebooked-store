fe0f970ba6df8c2e8e1935613d3355a2712a498a
Irvan Castro <castroirvan@gmail.com>
2025-03-08 01:01:00 +0800

    change cart state from array to object for better handling

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,14 +3,31 @@ import { createContext, useState, useContext } from "react";
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState({});
   
   const addToCart = (book) => {
-    setCart((prevCart) => [...prevCart, book]);
+    setCart((prevCart) => {
+      console.log('test')
+      const newCart = { ...prevCart };
+      if (newCart[book.id]) {
+        newCart[book.id].quantity += 1; // Increase quantity if book exists
+      } else {
+        newCart[book.id] = { ...book, quantity: 1 }; // Add new book
+      }
+      return newCart;
+    });
   };
-  
+
   const removeFromCart = (id) => {
-    setCart((prevCart) => prevCart.filter((book) => book.id !== id));
+    setCart((prevCart) => {
+      const newCart = { ...prevCart };
+      if (newCart[id].quantity > 1) {
+        newCart[id].quantity -= 1; // Decrease quantity if more than 1
+      } else {
+        delete newCart[id]; // Remove book if quantity reaches 0
+      }
+      return newCart;
+    });
   };
   
   return (
