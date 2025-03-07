f85493b6e2853299fbbe658a9ef9905b48fc706c
Irvan Castro <castroirvan@gmail.com>
2025-03-08 01:01:40 +0800

    refactor to match changes to cart handling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { useCart } from "../context/CartContext";
 
 function Header() {
   const { cart } = useCart();
+  const cartSize = Object.keys(cart).length;
   
   return (
     <header className="flex justify-center px-2">
@@ -36,9 +37,9 @@ function Header() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 hover:scale-110 md:hover:scale-100">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                 </svg>
-                {cart.length > 0 && (
+                {cartSize > 0 && (
                   <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full text-xs px-1.5 py-0.5">
-                    {cart.length}
+                    {cartSize}
                   </span>
                 )}
               </div>
