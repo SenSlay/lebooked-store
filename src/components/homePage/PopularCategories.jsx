f7ef5a60ccfcf26b533676cf5606db5e1a867d5b
Irvan Castro <castroirvan@gmail.com>
2025-03-10 00:58:41 +0800

    move to homePage folder

---
+++ src/components/homePage/PopularCategories.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const categoryImages = {
+  "Self-Help": "../images/category/self-help_category.png",
+  "Romance": "../images/category/romance_category.png",
+  "Science Fiction": "../images/category/science-fiction_category.png",
+  "Classic": "../images/category/classics_category.png"
+};
+
+function PopularCategories({ titles }) {
+  return (
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+      {titles.map((title, index) => (
+        <Link to={`/books?category=${title}`} key={index}>
+          <div className="w-full bg-slate-100 flex items-center flex-col rounded hover:shadow-xl">
+            <img src={categoryImages[title]} alt="Category" className="w-full h-[200px] bg-white mb-4 rounded-t"/>
+            <h1 className="font-medium text-lg mx-2 mb-4">{title}</h1>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+PopularCategories.propTypes = {
+  titles: PropTypes.array,
+}
+
+
+export default PopularCategories;
