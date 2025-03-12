6d460350e43193ca460dd91714f2820192458859
Irvan Castro <castroirvan@gmail.com>
2025-03-12 16:15:39 +0800

    make value lowercased for easier comparison

--- src/components/booksPage/FilterSection.jsx
+++ src/components/booksPage/FilterSection.jsx
@@ -3,18 +3,21 @@ export default function FilterSection({ title, options, selectedValue, onChange,
     <div className="py-5 border-t border-gray-400">
       <h2 className="text-lg mb-3 font-semibold text-gray-800">{title}</h2>
       <div className="flex flex-col">
-        {options.map(({ value, label }) => (
-          <label key={value} className="flex items-center gap-3 cursor-pointer hover:bg-slate-200 rounded p-2">
-            <input
-              type="checkbox"
-              name={title.toLowerCase()}
-              checked={isMulti ? selectedValue.includes(value.toLowerCase()) : selectedValue === value}
-              onChange={() => onChange(value)}
-              className="w-4 h-4 text-blue-600"
-            />
-            {label}
-          </label>
-        ))}
+        {options.map(({ value, label }) => {
+          const valueLowCased = value.toLowerCase();
+          return (
+            <label key={valueLowCased} className="flex items-center gap-3 cursor-pointer hover:bg-slate-200 rounded p-2">
+              <input
+                type="checkbox"
+                name={title.toLowerCase()}
+                checked={isMulti ? selectedValue.includes(valueLowCased) : selectedValue === valueLowCased}
+                onChange={() => onChange(valueLowCased)}
+                className="w-4 h-4 text-blue-600"
+              />
+              {label}
+            </label>
+          )
+        })}
       </div>
     </div>
   );
