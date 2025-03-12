4a9140faa742cbd315ab65b8d842195d97f1e48c
Irvan Castro <castroirvan@gmail.com>
2025-03-12 17:21:24 +0800

    add BookDetails page based on book id

--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Books from "./pages/Books";
 import ShoppingCart from "./pages/ShoppingCart";
 import ErrorPage from "./pages/ErrorPage";
+import BookDetails from "./pages/BookDetails";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
       { path: "/", element: <Home /> }, 
       { path: "/books", element: <Books /> },
       { path: "/cart", element: <ShoppingCart /> },
+      { path: "/books/:id", element: <BookDetails /> },
     ],
   },
 ]);
