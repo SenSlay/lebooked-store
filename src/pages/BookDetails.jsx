e357c38161f68b914affc0a8c5cc787529ea3cda
Irvan Castro <castroirvan@gmail.com>
2025-03-14 02:39:27 +0800

    add error detection when trying to add to cart with 0 quantity

--- src/pages/BookDetails.jsx
+++ src/pages/BookDetails.jsx
@@ -11,8 +11,19 @@ const BookDetails = () => {
   const [quantity, setQuantity] = useState(1);
   const { updateCartQuantity } = useCart();
   const { showModal } = useModal();
+  const [ error, setError ] = useState(null);
 
   const book = books.find((b) => b.id.toString() === id);
+
+  const handleAddToCart = () => {
+    if (quantity === 0) {
+      setError("Quantity must be at least 1.");
+      return;
+    }
+    setError(null);
+    updateCartQuantity(book, quantity, true);
+    showModal(`Book added to your cart`);
+  };
 
   if (!book) {
     return <p className="text-center text-red-500">Book not found!</p>;
@@ -25,14 +36,15 @@ const BookDetails = () => {
           <img src={book.image} alt={book.title} className="h-80 lg:h-96 object-cover" />
         </div>
         <div className="flex flex-col justify-center items-center text-center lg:justify-start lg:text-start lg:items-start">
-          <h1 className="text-3xl font-bold mt-4">{book.title}</h1>
+          <h1 className="text-3xl font-bold ">{book.title}</h1>
           <p className="text-gray-600 text-lg">By {book.author}</p>
           <p className="mt-4 mb-10 lg:mb-20">{book.description}</p>
           <p className="mt-2 mb-2 text-xl font-semibold">${book.price}</p>
 
           <QuantitySelector quantity={quantity} setQuantity={setQuantity} />
-          <div className="flex flex-col lg:flex-row gap-5">
-            <button onClick={() => {updateCartQuantity(book, quantity); showModal(`Book added to your cart`)}} className="flex gap-3 justify-center items-center py-2 w-[220px] font-medium rounded border-2 border-blue-600 bg-blue-50 text-black hover:bg-blue-100">
+          {error && <p className="text-red-500 text-sm mt-2 font-medium">{error}</p>}
+          <div className="flex flex-col lg:flex-row gap-5 mt-5">
+            <button onClick={handleAddToCart} className="flex gap-3 justify-center items-center py-2 w-[220px] font-medium rounded border-2 border-blue-600 bg-blue-50 text-black hover:bg-blue-100">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
               </svg>
