a25b293eb7724b32438beb330f022e9096d7c450
Irvan Castro <castroirvan@gmail.com>
2025-03-14 02:47:49 +0800

    wrap img element in a Link

--- src/components/shoppingCartPage/CartItem.jsx
+++ src/components/shoppingCartPage/CartItem.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useCart } from "../../context/CartContext";
 import QuantitySelector from "../common/QuantitySelector";
+import { Link } from "react-router-dom";
 
 const CartItem = ({ book }) => {
   const { updateCartQuantity, removeFromCart } = useCart();
@@ -14,7 +15,9 @@ const CartItem = ({ book }) => {
   return (
     <li key={book.id} className="grid sm:grid-cols-[1fr_0.5fr_1fr_0.5fr_90px] lg:grid-cols-[2fr_0.5fr_1fr_0.5fr_90px] grid-cols-[1fr_auto] gap-4 py-4 border-b items-center">
       <div className="flex items-start lg:items-center gap-3 flex-col lg:flex-row">
-        <img src={book.image} alt={book.title} className="object-cover h-40 lg:h-52" />
+        <Link to={`/books/${book.id}`}>
+          <img src={book.image} alt={book.title} className="object-cover h-40 lg:h-52 cursor-pointer hover:scale-105" />
+        </Link>
         <h2 className="text-lg font-semibold">{book.title}</h2>
       </div>
 
