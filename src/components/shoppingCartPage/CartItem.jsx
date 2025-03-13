33d44863f1cbdefd240950455fa621d8b59287b7
Irvan Castro <castroirvan@gmail.com>
2025-03-14 02:10:11 +0800

    improve styling and responsiveness

--- src/components/shoppingCartPage/CartItem.jsx
+++ src/components/shoppingCartPage/CartItem.jsx
@@ -12,21 +12,31 @@ const CartItem = ({ book }) => {
   }, [quantity, updateCartQuantity, book]); 
 
   return (
-    <li key={book.id} className="grid grid-cols-[2fr_1fr_1fr_1fr_90px] gap-4 py-2 border-b items-center">
+    <li key={book.id} className="grid sm:grid-cols-[1fr_0.5fr_1fr_0.5fr_90px] lg:grid-cols-[2fr_0.5fr_1fr_0.5fr_90px] grid-cols-[1fr_auto] gap-4 py-4 border-b items-center">
       <div className="flex items-start lg:items-center gap-3 flex-col lg:flex-row">
-        <img src={book.image} alt={book.title} className="object-cover h-40" />
+        <img src={book.image} alt={book.title} className="object-cover h-40 lg:h-52" />
         <h2 className="text-lg font-semibold">{book.title}</h2>
       </div>
 
-      <p className="font-bold">${book.price}</p>
+      <p className="font-medium hidden sm:block">${book.price}</p>
 
-      <QuantitySelector quantity={quantity} setQuantity={setQuantity} />
+      <div className="flex flex-col items-center sm:items-start">
+        <p className="font-medium mb-5 sm:hidden">${book.price}</p>
+        <QuantitySelector quantity={quantity} setQuantity={setQuantity} />
+        <p className="font-medium my-5 sm:hidden">${(book.price * quantity).toFixed(2)}</p>
+        <button 
+          onClick={() => removeFromCart(book.id)} 
+          className="px-3 py-1 bg-red-500 hover:bg-red-600 text-white rounded sm:hidden"
+        >
+          Remove
+        </button>
+      </div>
 
-      <p className="font-bold">${(book.price * quantity).toFixed(2)}</p>
+      <p className="font-medium hidden sm:block">${(book.price * quantity).toFixed(2)}</p>
 
       <button 
         onClick={() => removeFromCart(book.id)} 
-        className="px-3 py-1 bg-red-500 hover:bg-red-600 text-white rounded"
+        className="px-3 py-1 bg-red-500 hover:bg-red-600 text-white rounded hidden sm:block"
       >
         Remove
       </button>
