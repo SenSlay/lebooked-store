3720b2f256604b17b5dce9200a9b911cd64eb055
Irvan Castro <castroirvan@gmail.com>
2025-03-14 18:23:09 +0800

    make filter sections collapsable

--- src/components/booksPage/FilterSection.jsx
+++ src/components/booksPage/FilterSection.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
+
 export default function FilterSection({ title, options, selectedValue, onChange, isMulti = false }) {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div className="py-5 border-t border-gray-400">
-      <h2 className="text-lg mb-3 font-semibold text-gray-800">{title}</h2>
-      <div className="flex flex-col">
-        {options.map(({ value, label }) => {
-          const valueLowCased = value.toLowerCase();
-          return (
-            <label key={valueLowCased} className="flex items-center gap-3 cursor-pointer hover:bg-slate-200 rounded p-2">
-              <input
-                type="checkbox"
-                name={title.toLowerCase()}
-                checked={isMulti ? selectedValue.includes(valueLowCased) : selectedValue === valueLowCased}
-                onChange={() => onChange(valueLowCased)}
-                className="w-4 h-4 text-blue-600"
-              />
-              {label}
-            </label>
-          )
-        })}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="w-full flex justify-between items-center text-lg font-semibold text-gray-800 mb-3 focus:outline-none"
+      >
+        {title}
+        {isOpen ? 
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M11.47 7.72a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 1 1-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 0 1-1.06-1.06l7.5-7.5Z" clipRule="evenodd" />
+          </svg>
+        : 
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+            <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clipRule="evenodd" />
+          </svg>
+        }
+      </button>
+
+      <div
+        className={`transition-all duration-300 ease-in-out overflow-hidden ${isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"}`}
+      >
+        <div className="flex flex-col">
+          {options.map(({ value, label }) => {
+            const valueLowCased = value.toLowerCase();
+            return (
+              <label
+                key={valueLowCased}
+                className="flex items-center gap-3 cursor-pointer hover:bg-slate-200 rounded p-2"
+              >
+                <input
+                  type="checkbox"
+                  name={title.toLowerCase()}
+                  checked={isMulti ? selectedValue.includes(valueLowCased) : selectedValue === valueLowCased}
+                  onChange={() => onChange(valueLowCased)}
+                  className="w-4 h-4 text-blue-600"
+                />
+                {label}
+              </label>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
