37d89c7c638af917bab8e45d42f015cf8ef69893
Irvan Castro <castroirvan@gmail.com>
2025-03-14 17:38:29 +0800

    make search item a link and improve styling

--- src/components/common/SearchSidebar.jsx
+++ src/components/common/SearchSidebar.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { BooksContext } from "../../context/BooksContext";
+import { Link } from "react-router-dom";
 
 export default function SearchSidebar({ isOpen, onClose }) {
   const [searchQuery, setSearchQuery] = useState("");
@@ -12,7 +13,7 @@ export default function SearchSidebar({ isOpen, onClose }) {
 
   return (
     <div className={`fixed inset-0 z-50 bg-black bg-opacity-40 transition-opacity ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"}`} onClick={onClose}>
-      <div className={`fixed top-0 right-0 h-full w-96 bg-white shadow-lg transform transition-transform ${isOpen ? "translate-x-0" : "translate-x-full"}`} onClick={(e) => e.stopPropagation()}>
+      <div className={`fixed top-0 right-0 h-full w-80 md:w-[500px] bg-white shadow-lg transform transition-transform ${isOpen ? "translate-x-0" : "translate-x-full"}`} onClick={(e) => e.stopPropagation()}>
         <div className="p-5 flex justify-between items-center border-b">
           <h2 className="text-lg font-semibold">Search</h2>
           <button onClick={onClose} className="text-gray-500 hover:text-black">✕</button>
@@ -26,13 +27,18 @@ export default function SearchSidebar({ isOpen, onClose }) {
             className="w-full p-2 border rounded-md" 
           />
         </div>
-        <ul className="p-5 space-y-3">
+        <ul className="p-5 space-y-3 overflow-auto max-h-full">
           {searchQuery.trim() !== "" && filteredBooks.length > 0 ? (
             filteredBooks.map((book) => (
               <li key={book.id} className="border-b pb-2">
-                <p className="font-medium">{book.title}</p>
-                <p className="text-sm text-gray-500">{book.author}</p>
-              </li>
+                <Link to={`/books/${book.id}`} className="flex p-2 hover:bg-gray-100 rounded-md" onClick={onClose}>
+                  <img src={book.image} alt="" className="w-16 h-20 mr-5"/>
+                  <div>
+                    <p className="font-medium">{book.title}</p>
+                    <p className="text-sm text-gray-500">{book.author}</p>
+                  </div>
+                </Link>
+            </li>
             ))
           ) : searchQuery.trim() !== "" ? (
             <p className="text-gray-500">No results found.</p>
