8b7236cc88e2bf51382fda7e8ccaadaf1c91b074
Irvan Castro <castroirvan@gmail.com>
2025-03-14 15:35:44 +0800

    wrap Header component within BooksProvider as it will be used for Search Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
-import Header from "./components/Header"
-import Footer from "./components/Footer";
+import Header from "./components/common/Header"
+import Footer from "./components/common/Footer";
 import { BooksProvider } from "./context/BooksContext";
 import { CartProvider } from "./context/CartContext";
 import { ModalProvider } from "./context/ModalContext";
@@ -9,9 +9,9 @@ function App() {
   return (
     <CartProvider>
       <ModalProvider>
-        <Header />
         <BooksProvider>
-          <main className="flex-grow flex flex-col">
+          <Header />
+          <main className="flex-1 flex-grow flex flex-col">
             <Outlet />
           </main>
         </BooksProvider>
