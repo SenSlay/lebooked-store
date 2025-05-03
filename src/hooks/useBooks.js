bcf1a0513a4638143af993a67c9c71555502982b
Irvan Castro <castroirvan@gmail.com>
2025-05-03 21:22:49 +0800

    add ?limit=40 query to url link

--- src/hooks/useBooks.js
+++ src/hooks/useBooks.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/books/?limit=40';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/';
 
 const useBooks = (url = API_BASE_URL) => {
   const [books, setBooks] = useState([]);
@@ -9,7 +9,7 @@ const useBooks = (url = API_BASE_URL) => {
   useEffect(() => {
     const fetchBooks = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(`${url}/books/?limit=40`);
         if (!response.ok) throw new Error('Failed to fetch books');
 
         const data = await response.json();
