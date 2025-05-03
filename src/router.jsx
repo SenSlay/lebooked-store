5d6274682fd5e3b77f3d6efcb25839102b9f3348
Irvan Castro <castroirvan@gmail.com>
2025-05-03 21:24:21 +0800

    add routes to login and signup page

--- src/router.jsx
+++ src/router.jsx
@@ -5,6 +5,8 @@ import Books from './pages/Books';
 import ShoppingCart from './pages/ShoppingCart';
 import ErrorPage from './pages/ErrorPage';
 import BookDetails from './pages/BookDetails';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const router = createBrowserRouter([
   {
@@ -16,6 +18,8 @@ const router = createBrowserRouter([
       { path: '/books', element: <Books /> },
       { path: '/cart', element: <ShoppingCart /> },
       { path: '/books/:id', element: <BookDetails /> },
+      { path: '/login', element: <Login /> },
+      { path: '/signup', element: <Signup /> }
     ],
   },
 ]);
