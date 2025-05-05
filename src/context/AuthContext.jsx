58d895ea339ec0813155fff507aba41b5d44ee3c
Irvan Castro <castroirvan@gmail.com>
2025-05-05 14:29:01 +0800

    create AuthContext for authentication functions

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,39 @@
+// src/context/AuthContext.jsx
+import { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext({ user: null });
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
+
+  const [token, setToken] = useState(() => localStorage.getItem('token') || null);
+
+  const login = (userData, token) => {
+    setUser(userData);
+    setToken(token);
+    localStorage.setItem('user', JSON.stringify(userData));
+    localStorage.setItem('token', token);
+
+    console.log('User logged in:', userData);
+    console.log('Token:', token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, token, isLoggedIn: !!user && !!token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => useContext(AuthContext) || { user: null };
