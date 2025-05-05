ee0c00d747c6dcfec1ce15d1e5d89a1a6ad266a7
Irvan Castro <castroirvan@gmail.com>
2025-05-05 14:30:54 +0800

    pass user ID and token as props to CartPovider

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import Footer from './components/common/Footer';
 import { BooksProvider } from './context/BooksContext';
 import { CartProvider } from './context/CartContext';
 import { ModalProvider } from './context/ModalContext';
+import { useAuth } from './context/AuthContext';
 
 function App() {
+  const { user, token } = useAuth();
+
   return (
     <>
-      <CartProvider>
-        <ModalProvider>
+      <ModalProvider>
+        <CartProvider userId={user?.id} token={token}>
           <BooksProvider>
             <Header />
             <main className="flex-1 flex-grow flex flex-col">
               <Outlet />
             </main>
           </BooksProvider>
-        </ModalProvider>
-      </CartProvider>
+        </CartProvider>
+      </ModalProvider>
       <Footer />
     </>
   );
