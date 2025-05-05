3a8507455e19f76f7d1b68eaaa3b38aefe82ae17
Irvan Castro <castroirvan@gmail.com>
2025-05-05 14:29:39 +0800

    wrap RouterProvider with AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 );
