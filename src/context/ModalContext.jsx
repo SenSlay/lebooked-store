48a06fc32eec7554606c06c50d702169320b63c7
Irvan Castro <castroirvan@gmail.com>
2025-05-06 13:43:16 +0800

    refactor: make modal icon passable

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -11,10 +11,12 @@ export const useModal = () => useContext(ModalContext);
 export const ModalProvider = ({ children }) => {
   const [modalMessage, setModalMessage] = useState('');
   const [isVisible, setIsVisible] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
 
   // Show modal with a message
-  const showModal = (message) => {
+  const showModal = (message, content = null) => {
     setModalMessage(message);
+    setModalContent(content);
     setIsVisible(true);
 
     // Auto-hide after 2 seconds
@@ -30,23 +32,12 @@ export const ModalProvider = ({ children }) => {
           onClick={() => setIsVisible(false)}
         >
           <div
-            className="bg-gray-600 p-4 rounded-md shadow-md h-[120px] w-[300px] flex flex-col justify-center items-center"
+            className="bg-gray-200 p-4 rounded-md shadow-xl h-[120px] w-[300px] flex flex-col justify-center items-center text-center"
             onClick={() => setIsVisible(false)}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="size-20 text-white"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                clipRule="evenodd"
-              />
-            </svg>
-
-            <p className="text-lg font-semibold text-white">{modalMessage}</p>
+            {modalContent}
+            
+            <p className="text-lg font-semibold text-blue-700">{modalMessage}</p>
           </div>
         </div>
       )}
